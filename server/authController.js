d9769e9f9f8c5a1d408e3f5b2bd296120110aab0
gribashov <gribashovdeveloper@gmail.com>
2021-08-15 15:19:50 +0300

    add registration func body and bcrypt password

--- server/authController.js
+++ server/authController.js
@@ -1,11 +1,34 @@
 const User = require("./models/User");
 const Role = require("./models/Role");
+const bcrypt = require("bcrypt");
 
 class authController {
   async registration(req, res) {
     try {
+      const {username, password} = req.body;
+      // find user with username
+      const candidate = await User.findOne({username});
+      // is user finded -> print error
+      if (candidate) {
+        return res.status(400).json({message: "User already exists"});
+      }
+      // else -> hash password
+      const hashPassword = bcrypt.hashSync(password, 7);
+      // -> find role
+      const userRole = Role.findOne({value: "user"});
+      // -> create user
+      const user = new User({
+        username,
+        password: hashPassword,
+        roles: [userRole.value],
+      });
+      // -> save in db
+      await user.save();
+      // -> print successfully response
+      return res.json({message: "User registered successfully"});
     } catch (e) {
       console.error(e);
+      res.status(400).json({message: "Registration error"});
     }
   }
 
@@ -13,16 +36,18 @@ class authController {
     try {
     } catch (e) {
       console.error(e);
+      res.status(400).json({message: "Login error"});
     }
   }
 
   async getUsers(req, res) {
     try {
-      const userRole = new Role();
-      const adminRole = new Role({value: "admin"});
-      await userRole.save();
-      await adminRole.save();
-      res.json("server working...");
+      // add roles in db
+      // const userRole = new Role();
+      // const adminRole = new Role({value: "admin"});
+      // await userRole.save();
+      // await adminRole.save();
+      res.json("Server working...");
     } catch (e) {
       console.error(e);
     }
